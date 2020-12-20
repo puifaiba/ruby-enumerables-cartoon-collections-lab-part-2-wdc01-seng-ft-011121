55bc2f5da89e6cb41cb71eb05a1af83ea36c28ad
Boonie Attawut <puifai.ba@gmail.com>
2020-12-20 04:49:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,6 @@ end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
   planeteer_calls.find do |element|
-    valid_calls.include?(element)
+    valid_calls.include? element
   end
 end
